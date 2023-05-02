e0360cf8e86c994c8362eacd0a9489754d9f557f
Benjamin Garside (v1) <benjamin.garside@version1.com>
2023-05-02 16:34:39 +0100

    remove montly

--- runbook.tf
+++ runbook.tf
@@ -22,19 +22,10 @@ resource "azurerm_automation_schedule" "vm-start-stop" {
   automation_account_name = var.automation_account_name
   frequency               = each.value.frequency
   week_days               = each.value.frequency == "Week" ? each.value.week_days : null
-  month_days              = each.value.frequency == "Month" ? each.value.month_days : null
-  dynamic "monthly_occurrence" {
-    for_each = each.value.frequency == "Month" && each.value.month_days == null ? [1] : []
-    content {
-      day        = each.value.monthly_occurrence.day
-      occurrence = each.value.monthly_occurrence.occurrence
-    }
-  }
-
-  interval    = each.value.interval
-  timezone    = var.timezone
-  start_time  = "${formatdate("YYYY-MM-DD", timeadd(timestamp(), "24h"))}T${each.value.run_time}Z"
-  description = "Schedule to ${each.value.start_vm == true ? "start" : "stop"} vm at ${each.value.run_time}"
+  interval                = each.value.interval
+  timezone                = var.timezone
+  start_time              = "${formatdate("YYYY-MM-DD", timeadd(timestamp(), "24h"))}T${each.value.run_time}Z"
+  description             = "Schedule to ${each.value.start_vm == true ? "start" : "stop"} vm at ${each.value.run_time}"
 
   depends_on = [
     azurerm_automation_runbook.vm-start-stop
